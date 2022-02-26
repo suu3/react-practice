1a76fb88b4ca0b10d8c9e94a70e57817222d7d1a
suu3 <ahob789@ewhain.net>
2022-02-27 01:03:56 +0900

    [Board] css, editor

--- src/routes/Board/PostAdd.js
+++ src/routes/Board/PostAdd.js
@@ -1,14 +1,40 @@
 import React, { useState, useEffect } from 'react';
+import "../../css/Board/PostAdd.css"
 import { Link } from 'react-router-dom';
 
 import { postList } from './Data';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 const postAdd = (props) => {
 
 
     return(
-        <>
-        </>
+        <section className="post-add-section"> 
+            <h2>게시글 쓰기</h2>
+            <div className='editor-container'>
+                <input className="title-input" type='text' placeholder='제목' />
+                <CKEditor
+                editor={ClassicEditor}
+                data="<p>Hello from CKEditor 5!</p>"
+                onReady={editor => {
+                    // You can store the "editor" and use when it is needed.
+                    console.log('Editor is ready to use!', editor);
+                }}
+                onChange={(event, editor) => {
+                    const data = editor.getData();
+                    console.log({ event, editor, data });
+                }}
+                onBlur={(event, editor) => {
+                    console.log('Blur.', editor);
+                }}
+                onFocus={(event, editor) => {
+                    console.log('Focus.', editor);
+                }}
+                />
+            </div>
+            <button className="add-submit-button">입력</button>
+        </section>
     )
 
 }
