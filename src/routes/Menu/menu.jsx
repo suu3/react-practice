967b215c5c6d7ae65c3e0779f051a7b574a52cf8
suu3 <ahob789@ewhain.net>
2022-03-30 16:27:31 +0900

    [Menu] data 이동

--- src/routes/Menu/menu.jsx
+++ src/routes/Menu/menu.jsx
@@ -1,15 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../../css/Menu/menu.module.css";
 import MenuBoard from "./menuBoard";
 import Header from "./header";
 import "@fortawesome/fontawesome-free/js/all.js";
 
 const Menu = (props) => {
+  const [menus, setMenus] = useState({
+    1: {
+      id: 1,
+      name: "hamburger",
+      price: "5000",
+      quantity: 0,
+    },
+    2: {
+      id: 2,
+      name: "coke",
+      price: "2000",
+      quantity: 0,
+    },
+  });
+  const handleAdd = (menu) => {
+    setMenus((menus) => {
+      const updated = { ...menus };
+      updated[menu.id] = menu;
+      return updated;
+    });
+  };
+  const handleDelete = (menu) => {
+    setMenus((menus) => {
+      const updated = { ...menus };
+      delete updated[menu.id];
+      return updated;
+    });
+  };
+
+  const handleIncrement = (menu) => {
+    setMenus((menus) => {
+      const updated = { ...menus };
+      const quantity = updated[menu.id].quantity;
+      const updatedMenu = { ...menus[menu.id], quantity: quantity + 1 };
+      updated[menu.id] = updatedMenu;
+      return updated;
+    });
+  };
+  const handleDecrement = (menu) => {
+    setMenus((menus) => {
+      const updated = { ...menus };
+      const quantity = updated[menu.id].quantity;
+      const updatedMenu = {
+        ...menus[menu.id],
+        quantity: quantity === 0 ? 0 : quantity - 1,
+      };
+      updated[menu.id] = updatedMenu;
+      return updated;
+    });
+  };
   return (
     <div className={styles.menu_body}>
       <div className={styles.container}>
         <Header />
-        <MenuBoard />
+        <MenuBoard
+          menus={menus}
+          onAdd={handleAdd}
+          onDelete={handleDelete}
+          onIncrease={handleIncrement}
+          onDecrease={handleDecrement}
+        />
       </div>
     </div>
   );
--- src/routes/Menu/menuBoard.jsx
+++ src/routes/Menu/menuBoard.jsx
@@ -4,68 +4,29 @@ import MenuAddForm from "./menuAddForm";
 import styles from "../../css/Menu/menuBoard.module.css";
 
 const MenuBoard = (props) => {
-  const [menus, setMenus] = useState({
-    1: {
-      id: 1,
-      name: "hamburger",
-      price: "5000",
-      quantity: 0,
-    },
-    2: {
-      id: 2,
-      name: "coke",
-      price: "2000",
-      quantity: 0,
-    },
-  });
-  const handleAdd = (menu) => {
-    setMenus((menus) => {
-      const updated = { ...menus };
-      updated[menu.id] = menu;
-      return updated;
-    });
+  const onClickDelete = (menu) => {
+    props.onDelete(menu);
   };
-  const handleDelete = (menu) => {
-    setMenus((menus) => {
-      const updated = { ...menus };
-      delete updated[menu.id];
-      return updated;
-    });
+  const onClickIncrease = (menu) => {
+    props.onIncrease(menu);
   };
-
-  const handleIncrement = (menu) => {
-    setMenus((menus) => {
-      const updated = { ...menus };
-      const quantity = updated[menu.id].quantity;
-      const updatedMenu = { ...menus[menu.id], quantity: quantity + 1 };
-      updated[menu.id] = updatedMenu;
-      return updated;
-    });
+  const onClickDecrease = (menu) => {
+    props.onDecrease(menu);
   };
-  const handleDecrement = (menu) => {
-    setMenus((menus) => {
-      const updated = { ...menus };
-      const quantity = updated[menu.id].quantity;
-      const updatedMenu = {
-        ...menus[menu.id],
-        quantity: quantity === 0 ? 0 : quantity - 1,
-      };
-      updated[menu.id] = updatedMenu;
-      return updated;
-    });
+  const onClickAdd = (menu) => {
+    props.onAdd(menu);
   };
-
   return (
     <>
-      <MenuAddForm onAdd={handleAdd} />
+      <MenuAddForm onAdd={onClickAdd} />
       <ul className={styles.menus}>
-        {Object.keys(menus).map((key) => (
+        {Object.keys(props.menus).map((key) => (
           <MenuRow
             key={key}
-            menu={menus[key]}
-            onDelete={handleDelete}
-            onIncrease={handleIncrement}
-            onDecrease={handleDecrement}
+            menu={props.menus[key]}
+            onDelete={onClickDelete}
+            onIncrease={onClickIncrease}
+            onDecrease={onClickDecrease}
           />
         ))}
       </ul>
