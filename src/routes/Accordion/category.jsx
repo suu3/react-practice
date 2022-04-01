5b368e978a218951909fd70c6496fb72bac07a30
suu3 <ahob789@ewhain.net>
2022-04-02 00:18:32 +0900

    [Accodion] icon, padding

--- src/routes/Accordion/category.jsx
+++ src/routes/Accordion/category.jsx
@@ -9,15 +9,18 @@ const Category = ({ isOpen, menu, handleBtnClick }) => {
     <div className={styles.menu}>
       <div className={styles.title}>
         <span>{menu.title}</span>
-        <button onClick={onClickBtn} className={styles.btn}>
-          {isOpen ? (
-            <i className="fas fa-angle-up"></i>
-          ) : (
-            <i className="fas fa-angle-down"></i>
-          )}
+        <button key={Math.random()} onClick={onClickBtn} className={styles.btn}>
+          <i className={isOpen ? "fas fa-angle-up" : "fas fa-angle-down"}></i>
         </button>
       </div>
-      <div className={isOpen ? styles.content_open : styles.content_close}>
+      <div
+        className={isOpen ? styles.content_open : styles.content_close}
+        style={{
+          padding: `${(menu.id - 0.8) * 4}rem 2rem ${
+            (menu.id - 0.8) * 2
+          }rem 2rem`,
+        }}
+      >
         {menu.content}
       </div>
     </div>
